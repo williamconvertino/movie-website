346b8881fede91df70adc9caa24c137eddec07b5
William Convertino <williamconvertino@gmail.com>
2023-09-20 15:07:40 -0400

    Added an example component

---
+++ components/ExampleComponent.js
@@ -0,0 +1,13 @@
+//Note the capitalized file name
+export default function ExampleComponent () {
+    
+    const val = 1 + 1;
+    //You can do your code in the function
+    
+    return (
+        <div>
+            This is the example component's HTML. The value is {val}.
+            {/* You can put javascript code in curley brackets, like "val" above */}
+        </div>
+    )
+}
