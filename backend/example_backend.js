b5fd71901b9cb98b5040f2ad42ec5202e0d97cce
William Convertino <williamconvertino@gmail.com>
2023-09-20 14:54:49 -0400

    Added backend folder

---
+++ backend/example_backend.js
@@ -0,0 +1,9 @@
+const exampleFunction = () => {
+    return "It worked!"
+}
+
+const exampleAsyncFunction = () => {
+    return "It worked! (asyncronously!!)"
+}
+
+export { exampleAsyncFunction, exampleFunction };
