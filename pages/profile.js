ede04f1700ec2977dee4e20f65afc50c863f5363
Adrian Gutierrez <ag555@duke.edu>
2023-10-03 14:07:33 -0400

    Profile page set up

--- pages/profile.js
+++ pages/profile.js
@@ -1,27 +1,42 @@
-import Link from 'next/link';
-
-import ExampleComponent from '@components/ExampleComponent'; //Make sure to import any components you need
+import React, { useState, useEffect } from 'react';
 
 export default function ProfilePage() {
+  const [username, setUsername] = useState('');
+  
+  const [ratedMovies, setRatedMovies] = useState([]);
+
+  useEffect(() => {
+    //API request to fetch username
+    fetch('/api/user') //REPLACE WITH ACTUAL API URL
+      .then((response) => response.json())
+      .then((data) => {
+        setUsername(data.username);
+      })
+      .catch((error) => {
+        console.error('Error fetching user data:', error);
+      });
+
+    //API request to fetch rated movies
+    fetch('/api/rated-movies') //REPLACE WITH ACTUAL API URL
+      .then((response) => response.json())
+      .then((data) => {
+        setRatedMovies(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching rated movies:', error);
+      });
+  }, []);
+
   return (
-    <div className="container">
-        <p>
-            This is an example of making a new webpage. You just need to make a new .js file in the /pages directory, and then copy the code from this page.
-        </p>
-        <p>
-            The URL will be [websitename]/[filename], where websitename is the name of the website (316-project.netlify.app or localhost:3000), and filename is the name of the file (in this case, its called example.js).
-        </p>
-        <p>
-            Look at the URL for this page for reference.
-        </p>
-
-        <ExampleComponent />
-        {/* You can use the component like an HTML tag, or in the form shown above */}
-        
-        <Link href="/">
-        <a>[Return home]</a>
-        </Link>
-
+    <div>
+      <h1>Your Profile</h1>
+      <p>Username: {username}</p>
+      <h2>Previously Rated Movies</h2>
+      <ul>
+        {ratedMovies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
