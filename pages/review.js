ad5918b8836777dd515de24c21b467ed2133f502
Adrian Gutierrez <ag555@duke.edu>
2023-10-31 23:19:34 -0400

    basic review page working

--- pages/review.js
+++ pages/review.js
@@ -1,58 +1,74 @@
-import React, {
-    useState,
-  } from 'react';
+import React, { useState } from "react";
+
+import TopBar from "@components/TopBar";
 
 export default function ReviewForm() {
-  const [movieTitle, setMovieTitle] = useState('');
-  const [rating, setRating] = useState(0); 
-  const [comment, setComment] = useState('');
-  const [reviews, setReviews] = useState('');
+  const [movieTitle, setMovieTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [reviews, setReviews] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const newReview = { 
-      movieTitle: movieTitle, 
-      rating: rating, 
-      comment: comment 
+    const newReview = {
+      movieTitle: movieTitle,
+      rating: rating,
+      comment: comment,
     };
 
     setReviews([...reviews, newReview]);
 
-    setMovieTitle('');
+    setMovieTitle("");
     setRating(0);
-    setComment('');
-  }
+    setComment("");
+  };
 
   return (
     <div>
+      <h1>Review A Movie</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Movie Title:
-          <input type="text" value={movieTitle} onChange={(e) => setMovieTitle(e.target.value)} />
+          <input
+            type="text"
+            value={movieTitle}
+            onChange={(e) => setMovieTitle(e.target.value)}
+          />
         </label>
+
         <label>
           Rating:
-          <input type="number" min="0" max="10" value={rating} onChange={(e) => setRating(e.target.value)} />
+          <input
+            type="number"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+            min="0"
+            max="10"
+          />
         </label>
+
         <label>
           Comment:
-          <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+          <textarea
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
         </label>
+
         <button type="submit">Submit Review</button>
       </form>
-      
       <div>
-        <h2>Your Reviews</h2>
+        <h2>List of Reviews</h2>
         <ul>
           {reviews.map((review, index) => (
             <li key={index}>
-              <strong>Movie Title:</strong> {review.movieTitle}
-              <hr />
               <strong>Rating:</strong> {review.rating}
               <br />
               <strong>Comment:</strong> {review.comment}
               <br />
+              <strong>Movie Title:</strong> {review.movieTitle}
+              <hr />
             </li>
           ))}
         </ul>
