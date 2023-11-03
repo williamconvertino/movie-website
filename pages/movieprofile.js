5efa243e6c97225847b5147fea88ece94f643036
Alan Wang <alan.wang@Duke.edu>
2023-11-03 19:57:46 -0400

    updated user profile and movie profile pages

--- pages/movieprofile.js
+++ pages/movieprofile.js
@@ -1,38 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopBar from '@components/TopBar';
 
-const MovieProfile = ({ match }) => {
+const MovieProfile = ({ movie, onSave}) => {
     // conversations are hard-coded, tried to implement routing for movie attributes
-    const { name, releaseDate, genre, imageUrl } = match.params;
-
     return (
         <div>
             <TopBar />
             <div className="movie-profile">
-            <div className="movie-details">
-                <h1>{name}</h1>
-                <p>Released Date: {releaseDate}</p>
-                <p>Genre: {genre}</p>
-                <img src={imageUrl} alt={name} />
-            </div>
-            <div className="user-ratings">
-                <div className="user-conversations">
-                <h2>User Rating: 4.5/5</h2>
-                <div className="user-conversation">
-                    <h3>User 1</h3>
-                    <p>Rated: Movie Name</p>
-                    <p>Rating: 4.5/5</p>
-                    <p>Comments: insert here...</p>
+                <div className="movie-details">
+                    <h1>{movie.name}</h1>
+                    <p>Released Date: {movie.releaseDate}</p>
+                    <p>Genre: {movie.releaseDate}</p>
+                    <img src={movie.imageUrl} alt={movie.name} />
                 </div>
-                <div className="user-conversation">
-                    <h3>User 2</h3>
-                    <p>Rated: Movie Name</p>
-                    <p>Rating: 4.0/5</p>
-                    <p>Comments: insert here...</p>
+                <div className="user-ratings">
+                    <div className="user-conversations">
+                        <h2>User Rating: 4.5/5</h2>
+                        <div className="user-conversation">
+                            <h3>User 1</h3>
+                            <p>Rated: Movie Name</p>
+                            <p>Rating: 4.5/5</p>
+                            <p>Comments: insert here...</p>
+                    </div>
+                        <div className="user-conversation">
+                            <h3>User 2</h3>
+                            <p>Rated: Movie Name</p>
+                            <p>Rating: 4.0/5</p>
+                            <p>Comments: insert here...</p>
+                        </div>
+                    </div>
                 </div>
-                {/* Add more user conversations as needed */}
-                </div>
-            </div>
+                <button onClick={() => onSave(movie)}>Save Movie</button>
             </div>
     </div>
   );
--- pages/profile.js
+++ pages/profile.js
@@ -3,22 +3,20 @@ import React, {
   useState,
 } from 'react';
 
-import {
-  doc,
-  getDoc,
-} from 'firebase/firestore';
+import {doc, getDoc, collection, addDoc, query, where, getDocs, deleteDoc } from 'firebase/firestore';
 
 import { UserAuth } from '@components/context/AuthContext';
 import TopBar from '@components/TopBar';
 import { db } from '@firebase';
 
 export default function ProfilePage() {
-
-  
   const {user, profile, emailSignUp, emailSignIn, logOut} = UserAuth()
   
   const [ratedMovies, setRatedMovies] = useState([]);
   const [ratingMap, setRatingMap] = useState({});
+
+  const [savedMovies, setSavedMovies] = useState([]);
+  const [newMovie, setNewMovie] = useState('');
 
   const getRatedMovies = async () => {
     if (!user) return;
@@ -40,8 +38,45 @@ export default function ProfilePage() {
     setRatedMovies(movieList)
   }
 
+  const getSavedMovies = async () => {
+    if (!user) return;
+
+    const q = query(collection(db, 'savedMovies'), where('uid', '==', user.uid));
+    const querySnapshot = await getDocs(q);
+
+    const savedMovieList = [];
+
+    querySnapshot.forEach((doc) => {
+      const savedMovie = doc.data();
+      savedMovieList.push(savedMovie);
+    });
+
+    setSavedMovies(savedMovieList);
+  };
+
+  const handleAddMovie = async () => {
+    // Add a new movie to the saved movies list
+    if (newMovie.trim() === '') return;
+
+    const savedMovie = {
+      uid: user.uid,
+      movieName: newMovie,
+    };
+
+    await addDoc(collection(db, 'savedMovies'), savedMovie);
+    setNewMovie('');
+  };
+
+  const handleDeleteMovie = async (movieId) => {
+    // Delete the movie from the saved movies list
+    // Modify code with actual database for saved movies of each user
+    await deleteDoc(doc(db, 'savedMovies', movieId));
+    setSavedMovies(savedMovies.filter((movie) => movie.id !== movieId));
+  };
+
   useEffect(() => {
-    getRatedMovies()
+    getRatedMovies();
+    getSavedMovies();
   }, [, user]);
 
   return (
@@ -59,6 +94,31 @@ export default function ProfilePage() {
             <li key={movie.id}>{movie.name} : {ratingMap[movie.id]} stars</li>
           ))}
         </ul>
+
+        <div className="saved-movies">
+          <div className="movie-input-container">
+            <h2>Saved Movies</h2>
+            <ul>
+              {savedMovies.map((movie) => (
+                <li key={movie.id}>
+                  <span>{movie.movieName}</span>
+                  <button onClick={() => handleDeleteMovie(movie.id)}>
+                    Delete Movie
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <input
+                type="text"
+                placeholder="Enter a movie name"
+                value={newMovie}
+                onChange={(e) => setNewMovie(e.target.value)}
+              />
+              <button onClick={handleAddMovie}>Add Movie</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
