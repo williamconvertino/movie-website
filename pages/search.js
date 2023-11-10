cfa0b43898181428ab95233385910f34038fa7ed
Alan Wang <alan.wang@Duke.edu>
2023-11-09 20:02:55 -0500

    update movie search filter css

--- pages/search.js
+++ pages/search.js
@@ -16,33 +16,28 @@ export default function SearchPage() {
       <div className = "movie-search">
         <h1>Search Movies</h1>
         <SearchResults/>
-
         <button onClick={toggleAdvancedOptions}>
-          {showAdvancedOptions ? 'Hide Advanced Options' : 'Show Advanced Options'}
+            {showAdvancedOptions ? 'Hide Advanced Options' : 'Show Advanced Options'}
         </button>
 
         {showAdvancedOptions && (
-          <div className="advanced-options">
-            <h2>Advanced Search Options</h2>
-            <ul>
-              {/* Add criteria with checkboxes */}
-              <li>
-                <label>
-                  <input type="checkbox" /> Criterion 1
-                </label>
-              </li>
-              <li>
-                <label>
-                  <input type="checkbox" /> Criterion 2
-                </label>
-              </li>
-              {/* Add more criteria as needed */}
-            </ul>
-          </div>
-        )}
+            <div className="advanced-options">
+              <h2>Advanced Search Options</h2>
+              <ul>
+                <li>
+                  <label>
+                    <input type="checkbox" /> Criterion 1
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input type="checkbox" /> Criterion 2
+                  </label>
+                </li>
+              </ul>
+            </div>
+          )}
       </div>
-
-      
     </div>
   );
 }
