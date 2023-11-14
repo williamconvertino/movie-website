f302b14102ef2eb1e4aa05a7ad63e1c275dd6cbb
William Convertino <williamconvertino@gmail.com>
2023-11-13 21:12:34 -0500

    Added client like system

--- components/feed/FeedItem.js
+++ components/feed/FeedItem.js
@@ -3,13 +3,22 @@ import {
   useState,
 } from 'react';
 
+import Cookies from 'js-cookie';
+
+import { UserAuth } from '@components/context/AuthContext';
+
 export default function FeedItem ({ review, handleAddCommentClick}) {
     
     const [userName, setUserName] = useState('Loading...')
     const [movie, setMovie] = useState(null)
     const [rating, setRating] = useState('Loading...')
     const [time, setTime] = useState('Loading...')
+    const [numLikes, setNumLikes] = useState(review.numLikes)
+    const [numDislikes, setNumDislikes] = useState(review.numLikes)
     
+    const [likeState, setLikeState] = useState(0)
+
+    const {user, profile} = UserAuth()
 
     const populateData = async () => {
     
@@ -40,12 +49,66 @@ export default function FeedItem ({ review, handleAddCommentClick}) {
         } else {
             setTime(`${daysAgo} days ago`)
         }
+
+    }
+
+    const generateLikeValue = async () => {
+        if (!profile) return
+        let newLikeState = Cookies.get(`user-${profile.id}&review-${review.id}`)
+        if (!newLikeState) newLikeState = 0
+        setLikeState(newLikeState)
+    }
+
+    const ToggleLike = async () => {
+        if (!profile) return;
+        
+        let likeValue = Cookies.get(`user-${profile.id}&review-${review.id}`)
+
+        if (likeValue == 1) {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, 0)
+            setNumLikes(numLikes - 1)
+            setLikeState(0)
+        } else if (likeValue == -1) {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, 1)
+            setNumLikes(numLikes + 1)
+            setNumDislikes(numDislikes - 1)
+            setLikeState(1)
+        } else {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, 1)
+            setNumLikes(numLikes + 1)
+            setLikeState(1)
+        }
+
+    }
+
+    const ToggleDislike = async () => {
+        if (!profile) return;
+
+        let likeValue = Cookies.get(`user-${profile.id}&review-${review.id}`)
+
+        if (likeValue == -1) {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, 0)
+            setNumDislikes(numDislikes - 1)
+            setLikeState(0)
+        } else if (likeValue == 1) {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, -1)
+            setNumLikes(numLikes - 1)
+            setNumDislikes(numDislikes + 1)
+            setLikeState(-1)
+        } else {
+            Cookies.set(`user-${profile.id}&review-${review.id}`, -1)
+            setNumDislikes(numDislikes + 1)
+            setLikeState(-1)
+        }
     }
 
     useEffect(() => {
         populateData()
     }, [review])
 
+    useEffect(() => {
+        generateLikeValue()
+    }, [profile])
     
     return(
         <div>
@@ -56,6 +119,12 @@ export default function FeedItem ({ review, handleAddCommentClick}) {
                     <p>Rating: {rating}</p>
                     <p>Review: {review.content}</p>
                     <p>{time}</p>
+                    <div> 
+                        <div style={{cursor: "pointer"}} onClick={ToggleLike}>{numLikes} likes</div> 
+                        <div style={{cursor: "pointer"}} onClick={ToggleDislike}>{numDislikes} dislikes</div>
+                    </div>
+                    <p>{likeState == 1 && "(Liked)"}</p>
+                    <p>{likeState == -1 && "(Disliked)"}</p>
                 </div>
                 {/* <ul>
                     {review.comments.map((comment) => (
